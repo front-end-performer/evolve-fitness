799f1f3befe0d1a64335c388b32a9afbe8bca98a
Oleksii <sodolinskyi@gmail.com>
2023-07-20 21:53:18 +0200

    added bem utilitie

--- src/components/page-header/PageHeader.vue
+++ src/components/page-header/PageHeader.vue
@@ -1,15 +1,30 @@
 <script lang="ts">
 import { defineComponent } from "vue";
+import { useBem } from "@/utilities/bem";
 
 export default defineComponent({
   setup() {
-    return {};
+    const bem = useBem("PageHeader");
+
+    return { bem };
   },
 });
 </script>
 
 <template>
-  <div>
-    <h2>{{ $t("globals.greeting") }}</h2>
+  <div :class="bem()">
+    <h2 :class="bem('header')">{{ $t("globals.greeting") }}</h2>
   </div>
 </template>
+
+<style lang="scss" scoped>
+/** define PageHeader */
+
+.PageHeader {
+  border: 1px solid red;
+
+  &-header {
+    border: 1px solid green;
+  }
+}
+</style>
